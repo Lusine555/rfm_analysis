8e6c0c0b0bee79648eb60358382df70bbacea8b9
Lusine555 <57660793+Lusine555@users.noreply.github.com>
2022-12-20 03:56:15 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rfm_analysis',
+      version='0.1',
+      description='A package for RFM analysis',
+      url='https://github.com/Lusine555/rfm_analysis',
+      author='Lusine Babayan',
+      author_email='lusine_babayan@edu.aua.am',
+      license='MIT',
+      packages=['rfm_analysis'],
+      zip_safe=False)
